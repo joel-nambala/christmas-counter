b6ba9e5b4044e64bb3dce122a87f4b9e7c977011
PROF. JOEL <joel.n.wanjala@gmail.com>
2022-11-21 08:43:50 -0800

    Updated HTML and CSS

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 'use strict';
 
+const countDays = document.querySelector('.countdown-days');
+const countHours = document.querySelector('.countdown-hours');
+const countMinutes = document.querySelector('.countdown-minutes');
+const countSeconds = document.querySelector('.countdown-seconds');
+
 const addZero = function (num) {
   return num < 10 ? `0${num}` : num;
 };
 
 const countdown = function () {
+  // Get the future and present timestamp
   const future = new Date('2022 12 25 00:00:00').getTime();
   const today = new Date().getTime();
+
+  // Substratc the timestamps to get the gap
   const gap = future - today;
 
   // Get time
@@ -15,20 +23,29 @@ const countdown = function () {
   const hour = minute * 60;
   const day = hour * 24;
 
+  // Get the time remaining till christmass
   if (gap > 0) {
     const days = Math.floor(gap / day);
     const hours = Math.floor((gap % day) / hour);
     const minutes = Math.floor((gap % hour) / minute);
     const seconds = Math.floor((gap % minute) / second);
-    const counter = `${addZero(days)}: ${addZero(hours)}: ${addZero(
-      minutes
-    )}: ${addZero(seconds)}`;
 
-    document.querySelector('h1').textContent = counter;
+    // Update the counter and display to the UI
+    countDays.textContent = addZero(days);
+    countHours.textContent = addZero(hours);
+    countMinutes.textContent = addZero(minutes);
+    countSeconds.textContent = addZero(seconds);
+
+    const counter = `${addZero(days)} : ${addZero(hours)} : ${addZero(
+      minutes
+    )} : ${addZero(seconds)}`;
+
+    console.log(counter);
   } else {
+    // Counter has expired
     alert('Counter has expired/merry christmass');
   }
 };
 
-// countdown();
+// Call the counter function every second
 setInterval(countdown, 1000);
